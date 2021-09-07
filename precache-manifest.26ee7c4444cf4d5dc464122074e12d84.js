0b4a06547f92963bc82a60004beed8c9bd13fa1d
Имя Фамилия <mihay0113@gmail.com>
2021-09-07 13:56:40 +0300

    Updates

---
+++ precache-manifest.26ee7c4444cf4d5dc464122074e12d84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df9074647de1d285e770676b89093b80",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f3c1af206cbef310345d",
+    "url": "./static/css/main.819e4dd6.chunk.css"
+  },
+  {
+    "revision": "65e0bada929c03a4a899",
+    "url": "./static/js/2.fa9ab8dd.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.fa9ab8dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3c1af206cbef310345d",
+    "url": "./static/js/main.79228ff0.chunk.js"
+  },
+  {
+    "revision": "c7ae2510e9f54903a2ed",
+    "url": "./static/js/runtime-main.b51b247a.js"
+  }
+]);
